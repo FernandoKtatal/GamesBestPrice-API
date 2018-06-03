9d984c8e60abf09cb89f46a56a7f9521490b9218
Fernando <fernandosouza@cc.ci.ufpb.br>
2018-06-03 14:00:05 -0300

    new fields and pagination added

--- apiEng/apiMongo/models.py
+++ apiEng/apiMongo/models.py
@@ -3,10 +3,13 @@ from mongoengine import Document,fields
 
 class Game(Document):
     name = fields.StringField(required=True)
+    nameSteam = fields.StringField(required=False)
     price = fields.FloatField(required=True)
     priceSteam = fields.FloatField(required=False)
     link = fields.StringField(required=True)
+    linkSteam = fields.StringField(required=False)
     img = fields.StringField(required=True)
+    imgSteam = fields.StringField(required=False)
 
 
 
